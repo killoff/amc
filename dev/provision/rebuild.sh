3779cef09d3f417880832923c1d51605ab51798c
Max Gubar <maxim.gubar@gmail.com>
2015-09-02 19:33:08 +0200

    - changed rebuild strategy from uninstall to upgrade

--- dev/provision/rebuild.sh
+++ dev/provision/rebuild.sh
@@ -9,27 +9,4 @@ mkdir -p $LOG_DIR
 git pull >> $LOG_DIR/rebuild.log 2>&1
 composer install >> $LOG_DIR/rebuild.log 2>&1
 
-magento setup:uninstall --no-interaction >> $LOG_DIR/rebuild.log 2>&1
-
-mkdir -p $LOG_DIR
-
-magento setup:install \
---base-url=http://95.85.27.59:8000/ \
---backend-frontname=admin \
---db-engine=mysql \
---db-host=localhost \
---db-name=magento \
---db-user=root \
---db-password=password \
---admin-firstname=John \
---admin-lastname=Doe \
---admin-email=admin@example.com \
---admin-user=admin \
---admin-password=123123q \
---admin-use-security-key=0 \
---language=en_US \
---currency=USD \
---timezone=America/Chicago \
---cleanup-database \
---sales-order-increment-prefix="ORD$" \
---session-save=db >> $LOG_DIR/rebuild.log 2>&1
+magento setup:upgrade >> $LOG_DIR/rebuild.log 2>&1
