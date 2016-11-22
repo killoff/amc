507ee29a6d2086f243974a2d40e4d560a076f548
Dmitriy Volik <killoff@gmail.com>
2016-11-22 23:22:55 +0100

    invoice collection

--- app/code/Amc/Timetable/Controller/Adminhtml/Queue/Invoice.php
+++ app/code/Amc/Timetable/Controller/Adminhtml/Queue/Invoice.php
@@ -1,60 +1,81 @@
 <?php
 namespace Amc\Timetable\Controller\Adminhtml\Queue;
 
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Magento\Sales\Model\Service\InvoiceService;
 use Magento\Backend\App\Action;
-use Amc\Timetable\Model\ResourceModel\CustomerDue;
 use Magento\Framework\Controller\Result\JsonFactory;
 
-class Invoice extends \Magento\Backend\App\Action
+class Invoice extends Action
 {
-    /** @var CustomerDue */
-    private $customerDue;
+    /** @var OrderCollectionFactory */
+    private $orderCollectionFactory;
+
+    /** @var InvoiceService */
+    private $invoiceService;
 
     /** @var JsonFactory */
     private $resultJsonFactory;
 
     public function __construct(
-        CustomerDue $customerDue,
+        OrderCollectionFactory $orderCollectionFactory,
+        InvoiceService $invoiceService,
         JsonFactory $resultJsonFactory,
         Action\Context $context
     ) {
         parent::__construct($context);
-        $this->customerDue = $customerDue;
+        $this->orderCollectionFactory = $orderCollectionFactory;
+        $this->invoiceService = $invoiceService;
         $this->resultJsonFactory = $resultJsonFactory;
     }
 
-    /**
-     * Chooser Source action
-     *
-     * @return \Magento\Framework\Controller\Result\Raw
-     */
     public function execute()
     {
-        $customerId = $this->getRequest()->getParam('customer_id');
-        $orderItems = $this->customerDue->getOrderItemsDue($customerId);
-        $groupByOrders = $this->groupByOrders($orderItems);
-        /** @var \Magento\Framework\Controller\Result\Json $resultJson */
-        $resultJson = $this->resultJsonFactory->create();
-        return $resultJson->setData( array_values($groupByOrders) );
-    }
-
-    private function groupByOrders(array $orderItems)
-    {
         $result = [];
-        foreach($orderItems as $item) {
-            $orderId = $item['order_id'];
-            if (!isset($result[$orderId])) {
-                $result[$orderId] = [
-                    'order' => [
-                        'id' => $orderId,
-                        'increment_id' => $item['increment_id'],
-                        'created_at' => $item['created_at']
-                    ],
-                    'items' => []
+        $customerId = $this->getRequest()->getParam('customer_id');
+        /** @var \Magento\Sales\Model\ResourceModel\Order\Collection $orderCollection */
+        $orderCollection = $this->orderCollectionFactory->create();
+        $orderCollection
+            ->addFieldToFilter('customer_id', $customerId)
+            ->addFieldToFilter('state', [ 'in' => [Order::STATE_NEW, Order::STATE_PROCESSING] ])
+            ->setOrder('created_at', 'ASC');
+        /** @var Order $order */
+        foreach ($orderCollection->getItems() as $order) {
+            if (!$order->canInvoice()) {
+                continue;
+            }
+
+            $invoice = $this->invoiceService->prepareInvoice($order);
+
+            if (!$invoice->getTotalQty()) {
+                continue;
+            }
+            $invoiceItems = [];
+            /** @var \Magento\Sales\Model\Order\Invoice\Item $item */
+            foreach ($invoice->getItemsCollection() as $item) {
+                $invoiceItems[] = [
+                    'product_id' => $item->getProductId(),
+                    'order_item_id' => $item->getOrderItemId(),
+                    'name' => $item->getName(),
+                    'sku' => $item->getProductId(),
+                    'price' => $item->getPriceInclTax(),
+                    'row_total' => $item->getRowTotalInclTax(),
+                    'qty' => $item->getQty(),
                 ];
             }
-            $result[$orderId]['items'][] = $item;
+
+            $result[] = [
+                'order_id' => $order->getId(),
+                'increment_id' => $order->getIncrementId(),
+                'created_at' => $order->getCreatedAt(),
+                'items' => $invoiceItems,
+                'total' => $invoice->getGrandTotal(),
+                'discount_amount' => $invoice->getDiscountAmount()
+            ];
         }
-        return $result;
+        /** @var \Magento\Framework\Controller\Result\Json $resultJson */
+        $resultJson = $this->resultJsonFactory->create();
+        return $resultJson->setData( array_values($result) );
     }
 }
