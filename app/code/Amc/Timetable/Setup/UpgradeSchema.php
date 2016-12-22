c99d25b893bfe28c9f8bf0e7a42186f753a6ca44
Dmitry Volik <dmitry.volik@unic.com>
2016-12-22 09:47:04 +0100

    queue status

--- app/code/Amc/Timetable/Setup/UpgradeSchema.php
+++ app/code/Amc/Timetable/Setup/UpgradeSchema.php
@@ -54,6 +54,15 @@ class UpgradeSchema implements UpgradeSchemaInterface
         $installer = $setup;
         $installer->startSetup();
 
+        $this->createOrderEventTable($installer);
+        $this->createQueueStatusTable($installer);
+        $this->addAttributesToOrder();
+
+        $installer->endSetup();
+    }
+
+    private function createOrderEventTable($installer)
+    {
         $table = $installer->getConnection()->newTable(
             $installer->getTable('amc_timetable_order_event')
         )->addColumn(
@@ -120,8 +129,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
             'Order timetable events'
         );
         $installer->getConnection()->createTable($table);
-
-        $connection = $setup->getConnection();
+        $connection = $installer->getConnection();
         $connection->addColumn($installer->getTable('amc_timetable_order_event'), 'uuid', 'VARCHAR(50) NOT NULL AFTER room_id');
         $connection->addIndex(
             $installer->getTable('amc_timetable_order_event'),
@@ -129,7 +137,87 @@ class UpgradeSchema implements UpgradeSchemaInterface
             'uuid',
             \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
         );
+    }
 
+    private function createQueueStatusTable($installer)
+    {
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('amc_timetable_queue_status')
+        )->addColumn(
+            'entity_id',
+            Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true]
+        )->addColumn(
+            'customer_id',
+            Table::TYPE_INTEGER,
+            null,
+            ['unsigned' => true, 'nullable' => false]
+        )->addColumn(
+            'status',
+            Table::TYPE_SMALLINT,
+            null,
+            ['unsigned' => false, 'nullable' => false]
+        )->addColumn(
+            'changed_by',
+            Table::TYPE_INTEGER,
+            null,
+            ['unsigned' => true, 'nullable' => true]
+        )->addColumn(
+            'changed_at',
+            Table::TYPE_DATETIME,
+            null,
+            ['nullable' => false]
+        )->addColumn(
+            'end_at',
+            Table::TYPE_DATETIME,
+            null,
+            ['nullable' => false]
+        )->addColumn(
+            'order_item_id',
+            Table::TYPE_INTEGER,
+            null,
+            ['unsigned' => true, 'nullable' => false]
+        )->addColumn(
+            'room_id',
+            Table::TYPE_INTEGER,
+            null,
+            ['unsigned' => true, 'nullable' => false],
+            'Room ID'
+        )->addForeignKey(
+            $installer->getFkName('amc_timetable_order_event', 'user_id', 'admin_user', 'user_id'),
+            'user_id',
+            $installer->getTable('admin_user'),
+            'user_id',
+            Table::ACTION_NO_ACTION
+        )->addForeignKey(
+            $installer->getFkName('amc_timetable_order_event', 'customer_id', 'customer_entity', 'entity_id'),
+            'customer_id',
+            $installer->getTable('customer_entity'),
+            'entity_id',
+            Table::ACTION_CASCADE
+        )->addForeignKey(
+            $installer->getFkName('amc_timetable_order_event', 'order_item_id', 'sales_order_item', 'item_id'),
+            'order_item_id',
+            $installer->getTable('sales_order_item'),
+            'item_id',
+            Table::ACTION_CASCADE
+        )->addForeignKey(
+            $installer->getFkName('amc_timetable_order_event', 'room_id', 'amc_clinic_room_entity', 'entity_id'),
+            'room_id',
+            $installer->getTable('amc_clinic_room_entity'),
+            'entity_id',
+            Table::ACTION_NO_ACTION
+        )->setComment(
+            'Order timetable events'
+        );
+        $installer->getConnection()->createTable($table);
+
+
+    }
+
+    private function addAttributesToOrder()
+    {
         $salesInstaller = $this->salesSetupFactory->create(['resourceName' => 'sales_setup', 'setup' => $this->moduleDataSetup]);
         $salesInstaller->addAttribute(
             'order',
@@ -141,34 +229,6 @@ class UpgradeSchema implements UpgradeSchemaInterface
             'timetable_end_at',
             ['type' => 'datetime', 'visible' => false, 'default' => null]
         );
-
-        /** @var CustomerSetup $customerSetup */
-        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
-        $attributesInfo = [
-            'timetable_status' => [
-                'label' => 'Timetable Status',
-                'type' => 'static',
-                'input' => 'text',
-                'visible' => false,
-                'required' => false,
-            ],
-        ];
-
-        foreach ($attributesInfo as $attributeCode => $attributeParams) {
-            $customerSetup->addAttribute('customer', $attributeCode, $attributeParams);
-        }
-        $setup->getConnection()->addColumn(
-            $setup->getTable('customer_entity'),
-            'timetable_status',
-            [
-                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
-                'nullable' => false,
-                'default'  => '0',
-                'unsigned' => true,
-                'comment'  => 'Timetable Status'
-            ]
-        );
-
-        $installer->endSetup();
     }
+
 }
