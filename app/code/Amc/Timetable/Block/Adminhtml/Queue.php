b32413f6c3951919d9f6dbe417aae79fa07bb51d
Dmitriy Volik <killoff@gmail.com>
2017-01-20 00:32:22 +0100

    Incorrect dependency: UrlInterface already exists in context object

--- app/code/Amc/Timetable/Block/Adminhtml/Queue.php
+++ app/code/Amc/Timetable/Block/Adminhtml/Queue.php
@@ -2,33 +2,19 @@
 namespace Amc\Timetable\Block\Adminhtml;
 
 use Magento\Backend\Block\Template;
-use Magento\Framework\UrlInterface;
 
 class Queue extends Template
 {
-    /** @var UrlInterface */
-    private $urlBuilder;
-
-    public function __construct(
-        Template\Context $context,
-        UrlInterface $urlBuilder,
-        array $data
-    )
-    {
-        parent::__construct($context, $data);
-        $this->urlBuilder = $urlBuilder;
-    }
-
     public function getComponentOptionsJson()
     {
         $options = [
             'component' => 'Amc_Timetable/js/queue',
             'template' => 'Amc_Timetable/queue',
-            'source_url' => $this->urlBuilder->getUrl('timetable/queue/jsonFeed', ['date' => $this->getTodayDate()->format('Y-m-d')]),
-            'invoice_url' => $this->urlBuilder->getUrl('timetable/queue/invoice'),
-            'pay_url' => $this->urlBuilder->getUrl('timetable/queue/invoice'),
-            'change_status_url' => $this->urlBuilder->getUrl('timetable/queue/changeStatus'),
-            'edit_customer_url_prefix' => $this->urlBuilder->getUrl('customer/index/edit') . 'id/',
+            'source_url' => $this->_urlBuilder->getUrl('timetable/queue/jsonFeed', ['date' => $this->getTodayDate()->format('Y-m-d')]),
+            'invoice_url' => $this->_urlBuilder->getUrl('timetable/queue/invoice'),
+            'pay_url' => $this->_urlBuilder->getUrl('timetable/queue/invoice'),
+            'change_status_url' => $this->_urlBuilder->getUrl('timetable/queue/changeStatus'),
+            'edit_customer_url_prefix' => $this->_urlBuilder->getUrl('customer/index/edit') . 'id/',
         ];
 
         return \Zend_Json::encode($options);
